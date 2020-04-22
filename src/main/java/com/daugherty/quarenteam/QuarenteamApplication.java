f27e72d3f0234284d75850d0aa06c05a403c9681
Joseph Dasuki <josephdasuki@MSP42JLD061022.local>
2020-04-22 12:58:12 -0500

    Initial Spring boot app

---
+++ src/main/java/com/daugherty/quarenteam/QuarenteamApplication.java
@@ -0,0 +1,13 @@
+package com.daugherty.quarenteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarenteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuarenteamApplication.class, args);
+	}
+
+}
